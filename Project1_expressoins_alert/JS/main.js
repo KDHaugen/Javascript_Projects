7f7b9b4c53e2dc207174210a9cb341db1d3fe150
PANDABOMB-RoA <64565446+PANDABOMB-RoA@users.noreply.github.com>
2021-08-26 15:58:29 -0700

    Additions with variables and expressions added to main.js

---
+++ Project1_expressoins_alert/JS/main.js
@@ -0,0 +1,11 @@
+var A = "\"This is a string\""
++"This is a concatenated string";
+window.alert(A);
+document.write("Hello world!"
++"Good bye world!");
+document.write(A);
+
+var dog = "Balto", cat = "Ollie";
+var dog = dog.fontcolor("blue"), cat = cat.fontcolor("red");
+document.write(dog);
+document.write(5%2);
