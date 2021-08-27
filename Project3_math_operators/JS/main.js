4ffdcbbed5cbfa8aa1ce79de0e01344900115f12
PANDABOMB-RoA <64565446+PANDABOMB-RoA@users.noreply.github.com>
2021-08-26 21:21:56 -0700

    working on math functions

--- Project3_math_operators/JS/main.js
+++ Project3_math_operators/JS/main.js
@@ -1,7 +1,14 @@
-function math_function(){                //Defining a function and giving it a name.
+function math_function(){                //Defining a function and giving it a name math_function().
 
-    var mathOperatorA = 5%2;               //Creating a operator and name it mathOperatorA
+    var mathOperatorA = 5%2;               //Creating an operator and name it mathOperatorA
 
     
-    document.getElementById("Math").innerHTML = "5%2 = " + mathOperatorA; //Putting the value of outcome in the HTML element with "Math"
+    document.getElementById("Math").innerHTML = "5%2 = " + mathOperatorA; //Putting the value of MathOperatorA in the HTML element with "Math"
+}
+
+function subtraction_Function(){        //Defining a function and giving it the name subtraction_Function().
+
+    var Subtraction = 10 - 5;           //Creating an operator and named it Subtraction
+
+    document.getElementById("Math-sub").innerHTML = "10 - 5 =" + Subtraction;   //Putting the value of Subtraction in the HTML element with "Math-sub"
 }
