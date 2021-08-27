47198ee030449a6a9d9a45396add6027252cf3c6
PANDABOMB-RoA <64565446+PANDABOMB-RoA@users.noreply.github.com>
2021-08-27 14:23:14 -0700

    add coercion Operator

---
+++ Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,6 @@
+function Coercion_function(){                //Defining a function and giving it a name Coercion_function().
+
+    var coercion_Operator = "10" + 5;          
+
+    document.getElementById("Coercion").innerHTML = coercion_Operator; //Putting the value of Champion.name in the HTML element with "Dictionary"
+}
