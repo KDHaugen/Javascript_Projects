96085d5425996266462154e637614f8589a35da5
PANDABOMB-RoA <64565446+PANDABOMB-RoA@users.noreply.github.com>
2021-08-26 21:08:19 -0700

    math operator project added

---
+++ Project3_math_operators/JS/main.js
@@ -0,0 +1,7 @@
+function math_function(){                //Defining a function and giving it a name.
+
+    var mathOperatorA = 5%2;               //Creating a operator and name it mathOperatorA
+
+    
+    document.getElementById("Math").innerHTML = "5%2 = " + mathOperatorA; //Putting the value of outcome in the HTML element with "Math"
+}
