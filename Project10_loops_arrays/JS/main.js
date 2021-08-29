bb6f069a85ddafa4756b55cd6dd7266701efa0de
PANDABOMB-RoA <64565446+PANDABOMB-RoA@users.noreply.github.com>
2021-08-29 15:04:00 -0700

    projec t10

--- Project10_loops_arrays/JS/main.js
+++ Project10_loops_arrays/JS/main.js
@@ -18,3 +18,46 @@ function for_Loop() {
     }
     document.getElementById("List_of_Instruments").innerHTML = Content;
 }
+
+
+function array_Function(){
+    var champion_Name = [];
+    champion_Name[0] = "Lux";
+    champion_Name[1] = "Garen";
+    champion_Name[2] = "Morgana";
+    champion_Name[3] = "Elise";
+    champion_Name[4] = "Kiella";
+    document.getElementById("Array").innerHTML = "The name of a champion is " + champion_Name[2] + ".";
+}
+
+function constant_function() {
+    const X = 10;
+    //X = 100;
+    document.getElementById("Constant").innerHTML = "5 + 5 = " + X;
+}
+
+function let_function(){
+    var L = 100;
+    document.getElementById("Let_show").innerHTML = L + "<br>";
+    {
+        let L = 200;
+        document.getElementById("Let_show1").innerHTML = L + "<br>";
+    }
+    document.getElementById("Let_show2").innerHTML = L;
+}
+
+/*function return_function(){
+    return 10;
+}
+document.getElementById("return_show").innerHTML = return_function();*/
+
+
+let dog = {
+    breed: "Husky",
+    size: "large",
+    age: "4",
+    description : function(){
+        return "This dog is a " + this.size + "," + this.age + "year old," + this.breed + ".";
+    }
+};
+document.getElementById("dog_object").innerHTML = dog.description();
